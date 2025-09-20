586dcccea40733d1f60f8030f6834d89974dceb5
Rikard Carlsson <rikard.carlsson@iths.se>
2025-09-20 17:56:11 +0200

    Klar med ekvationsuppgiften

--- Övning 4.15/Program.cs
+++ Övning 4.15/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Specialized;
+﻿using System;
 
 namespace Övning_4._15
 {
@@ -6,31 +6,49 @@ namespace Övning_4._15
 	{
 		static void Main(string[] args)
 		{
-			int noOfSeconds = 0;
+			int a, b, c;
+			double p, q, d, x1, x2;
 
-			while (true) 
-				{ 
-				Console.Write("Ange antal sekunder: ");
-				noOfSeconds = int.Parse(Console.ReadLine());
-				if (noOfSeconds <= 60) 
+			Console.WriteLine("Välkommen till programmet som löser ax\u00B2 + bx + c = 0");
+			Console.WriteLine();
+
+			while (true)
+			{
+				Console.Write("Mata in värdet för a: ");
+				a = Convert.ToInt32(Console.ReadLine());
+				Console.Write("Mata in värdet för b: ");
+				b = Convert.ToInt32(Console.ReadLine());
+				Console.Write("Mata in värdet för c: ");
+				c = Convert.ToInt32(Console.ReadLine());
+
+				// Sätt ekvationen i rätt form
+				p = (double)b / a;
+				q = (double)c / a;
+				
+				// Räkna ut diskriminanten
+				d = Math.Pow(p / 2, 2) - q;
+
+				if (a == 0) 
 				{
-				    Console.WriteLine($"{noOfSeconds} är {noOfSeconds} sekunder");
+					Console.WriteLine("Detta är ingen andragradsekvation");
 				}
-				else if(noOfSeconds > 60 && noOfSeconds <= 3600) 
-				{ 
-					int noOfMinutes = noOfSeconds / 60;
-					int noOfSecondsRest = noOfSeconds % 60;
-					Console.WriteLine($"Det är {noOfMinutes} minuter och {noOfSecondsRest} sekunder.");
-				}
-				else if (noOfSeconds > 3600)
+				else if (d < 0)
 				{
-					int noOfHours = noOfSeconds / 3600;
-					int rest = noOfSeconds % 3600;
-					int noOfMinutes = rest / 60;
-					int noOfSecondsRest = rest % 60;
-					Console.WriteLine($"Det är {noOfHours} timmar, {noOfMinutes} minuter och {noOfSecondsRest} sekunder.");
+					Console.WriteLine($"Diskriminanten är {d:F2}, alltså under 0, reell lösning saknas, avbryter");
 				}
+				else 
+				{ 
+					x1 = -p / 2 + Math.Sqrt(d);
+					x2 = - p / 2 - Math.Sqrt(d);
+					string termA = a == 1 ? "x²" : a == -1 ? "-x²" : $"{a}x²";
+					string termB = b == 0 ? "" : (b == 1 ? "+ x" : b == -1 ? "- x" : (b > 0 ? $"+ {b}x" : $"- {Math.Abs(b)}x"));
+					string termC = c == 0 ? "" : (c > 0 ? $"+ {c}" : $"- {Math.Abs(c)}");
+					Console.WriteLine($"Ekvationen {termA} {termB} {termC} = 0 ger svaren: " );
+					Console.WriteLine($"X1 = {x1:F2}, X2 = {x2:F2}");
+				}
+
 			}
+
 		}
 	}
 }
