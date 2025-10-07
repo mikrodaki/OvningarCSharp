dee3e6efd11c6e66184dca67f4dbf791be812a3c
Rikard Carlsson <rikard.carlsson@iths.se>
2025-10-07 07:38:33 +0200

    Sten, sax

---
+++ Övning 4.18/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Övning_4._18
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+			while (true) 
+			{ 
+				string answer = "";
+				int resultFromDivision = 0;
+				int rest;
+				Console.WriteLine("Välkommen till programmet som konverterar" +
+					"ett decimaltal mellan 0-64 till binärt");
+				Console.WriteLine("-------");
+				Console.Write("Ange ett tal mellan 0-64: ");
+				int submittedNumber = Convert.ToInt32(Console.ReadLine());
+				resultFromDivision = submittedNumber;
+
+				while (resultFromDivision != 0) 
+				{
+					resultFromDivision = resultFromDivision / 2;
+					rest = resultFromDivision % 2;
+					answer = answer + rest.ToString();
+				}
+			}
+			
+		}
+	}
+}
---
+++ Övning 5.12/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Övning_5._12
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Välkommen till sten, sax, påse");
+			Console.WriteLine("------------------");
+			int playerScore = 0;
+			int cmpScore = 0;
+			int cmpChoice = 0;
+			int playerChoice = 0;
+			var rnd = new Random();
+
+			for (int i = 0; i < 3; i++)
+			{
+				cmpChoice = rnd.Next(1, 4); // 1, 2 eller 3
+				Console.Write("Ange 1 för sten, 2 för sax eller 3 för påse: ");
+				string input = Console.ReadLine();
+				if (int.TryParse(input, out playerChoice))
+				{
+					if (playerChoice >= 1 && playerChoice <= 3)
+					{
+						switch (playerChoice)
+						{
+							case (1):
+								Console.WriteLine($"Du valde {playerChoice} (sten) och datorn valde {cmpChoice}");
+								break;
+							case (2):
+								Console.WriteLine($"Du valde {playerChoice} (sax) och datorn valde {cmpChoice}");
+								break;
+							case (3):
+								Console.WriteLine($"Du valde {playerChoice} (påse) och datorn valde {cmpChoice}");
+								break;
+						}
+					}
+					else 
+					{ 
+						Console.WriteLine("Ogiltigt val, du förlorade rundan");
+						cmpScore++;
+					}
+				}
+				else
+				{
+					Console.WriteLine("Ogiltigt val, du förlorade rundan");
+					cmpScore++;
+				}
+			}
+
+			string calculateScore (int choice1, int choice2) 
+			{
+				if (choice1 == choice2)
+					return "draw";
+				return "";
+			}
+		}
+	}
+}
