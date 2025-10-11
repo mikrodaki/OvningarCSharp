15365327886b4f6dc789a0eeb226a6d7b9541637
Rikard Carlsson <rikard.carlsson@iths.se>
2025-10-11 21:09:21 +0200

    Italienska flaggan

---
+++ Övning 5.19/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Övning_5._19
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int y = 5;
+			int x = Console.WindowWidth / 2 - 30;
+
+			Console.BackgroundColor = ConsoleColor.Gray;
+			Console.Clear();
+
+			for (int i = 0; i < 18; i++)
+			{
+				Console.SetCursorPosition(x, y);
+				for (int j = 0; j < 60; j++)
+				{
+					if (j < 20)
+						Console.BackgroundColor = ConsoleColor.Green;
+					else if (j < 40)
+						Console.BackgroundColor = ConsoleColor.White;
+					else 
+						Console.BackgroundColor = ConsoleColor.Red;
+					Console.Write(" ");
+				}
+				Console.WriteLine();
+				y++;
+			}
+			Console.ReadKey();
+		}
+	}
+}
+
