6c8f6d69f6764793c1b1a150985824932bbc9513
Rikard Carlsson <rikard.carlsson@iths.se>
2026-01-03 14:00:50 +0100

    Division

---
+++ Övning 10.2 - Division/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Övning_10._2___Division
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			double numerator = 0;
+			double denominator = 0;
+			bool invalidInput = true;
+
+			do
+			{
+				try
+				{
+					Console.Write("Ange täljaren: ");
+					numerator = Convert.ToDouble(Console.ReadLine());
+					invalidInput = false;
+				}
+				catch (Exception)
+				{
+					Console.WriteLine("Täljaren måste vara ett heltal eller ett decimaltal. Försök igen. ");
+				}
+			}
+			while (invalidInput);
+
+			invalidInput = true;
+
+			do
+			{
+				try
+				{
+					Console.Write("Ange nämnaren: ");
+					denominator = Convert.ToDouble(Console.ReadLine());
+					if (denominator != 0)
+					{
+						invalidInput = false;
+					}
+					else 
+					{ 
+						Console.WriteLine("Nämnaren får inte vara 0. Försök igen. ");
+					}
+				}
+				catch (Exception)
+				{
+					Console.WriteLine("Nämnaren måste vara ett heltal eller ett decimaltal. Försök igen. ");
+				}
+			}
+			while (invalidInput);
+
+
+			double fraction = numerator / denominator;
+			Console.Write($"Kvoten av {numerator} / {denominator} är {fraction}  ");
+
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Övning 10e/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Övning_10e
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			bool notANumber = true;
+			do
+			{
+				Console.Clear();
+				Console.Write("Ange ett tal: ");
+				var input = Console.ReadLine();
+				try
+				{
+					int number = int.Parse(input);
+					notANumber = false;
+				}
+				catch (Exception e)
+				{
+					Console.Write("Ange endast tal. Försök igen. ");
+					Console.ReadKey();
+				}
+			} while (notANumber);
+
+			Console.Write("Du har angivit ett giltigt tal");
+		}
+	}
+}
