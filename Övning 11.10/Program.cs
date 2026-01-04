eb7f3e6e40c9e8043ec1b025e6acace918545fd3
Rikard Carlsson <rikard.carlsson@iths.se>
2026-01-04 15:49:18 +0100

    Metoder början

---
+++ Övning 11.10/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Övning_11._10
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int firstNumber = CheckIntegerInput("Ange tal 1: ");
+			int secondNumber = CheckIntegerInput("Ange tal 2: ");
+			int thirdNumber = CheckIntegerInput("Ange tal 3: ");
+			Console.WriteLine($"Största talet är: {Biggest(firstNumber, secondNumber, thirdNumber)}");
+		}
+
+		static int Biggest(int a, int b, int c)
+		{
+			int biggest = a;
+			if (b > biggest)
+				biggest = b;
+			if (c > biggest)
+				biggest = c;
+			return biggest;
+		}
+
+		static int CheckIntegerInput(string message)
+		{
+			int result = 0;
+			Console.Write(message);
+			while (!int.TryParse(Console.ReadLine(), out result))
+			{
+				Console.WriteLine("Felaktigt inmatning. Försök igen.");
+				Console.Write(message);
+			}
+			return result;
+		}
+	}
+}
