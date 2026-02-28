3edfd6f6c334a299318dec9a21e455968b4f7b5b
Rikard Carlsson <rikard.carlsson@iths.se>
2026-03-01 00:23:06 +0100

    Poker

---
+++ 12.18 - Poker/ConsoleHelper.cs
@@ -0,0 +1,180 @@
+﻿namespace App.IO
+{
+	internal class ConsoleHelper
+	{
+
+		public static int ReadInt(string prompt)
+		{
+			while (true)
+			{
+				Console.Write(prompt);
+				string? input = Console.ReadLine();
+
+				if (!int.TryParse(input, out int value))
+				{
+					Console.WriteLine("Fel: ange ett heltal.");
+					continue;
+				}
+
+				return value;
+			}
+		}
+
+		public static int ReadInt(string prompt, bool mustBeGreaterThanZero)
+		{
+			while (true)
+			{
+				Console.Write(prompt);
+				string? input = Console.ReadLine();
+
+				if (!int.TryParse(input, out int value))
+				{
+					Console.WriteLine("Fel: ange ett heltal.");
+					continue;
+				}
+
+				if (mustBeGreaterThanZero && value < 1)
+				{
+					Console.WriteLine("Fel: värdet måste vara större än 0.");
+					continue;
+				}
+
+				return value;
+			}
+		}
+
+		public static int ReadInt(string text, int min, int max)
+		{
+			while (true)
+			{
+				Console.Write(text);
+				string? input = Console.ReadLine();
+
+				if (!int.TryParse(input, out int value))
+				{
+					Console.WriteLine("Fel: ange ett heltal.");
+					continue;
+				}
+
+				if (value < min || value > max)
+				{
+					Console.WriteLine($"Fel: värdet måste vara inom intervallet {min} och {max}");
+					continue;
+				}
+
+				return value;
+			}
+		}
+
+		// To use in the poker program to read indexes
+		public static string ReadStringIntPoker(string text, int max)
+		{
+			List<int> indexes = new List<int>();
+
+			while (true)
+			{
+				Console.Write(text);
+				string? input = Console.ReadLine();
+
+				if (!string.IsNullOrEmpty(input))
+				{
+					bool ok = true;
+
+					for (int i = 0; i < input.Length; i++)
+					{
+						char c = input[i];
+						if (!char.IsDigit(c))
+						{
+							ok = false;
+							break;
+						}
+						else
+						{
+							var number = c - '0';
+							if (number < 0 || number > max)
+							{
+								ok = false;
+								break;
+							}
+							if (indexes.Contains(number))
+							{
+								ok = false;
+								break;
+							}
+							else
+								indexes.Add(number);
+						}
+					}
+
+					if (ok)
+						return input;
+				}
+
+				Console.WriteLine("Felaktig inmatning!");
+				indexes.Clear();
+			}
+		}
+
+		public static string ReadString(string text)
+		{
+			while (true)
+			{
+				Console.Write(text);
+				string? input = Console.ReadLine();
+
+				if (!string.IsNullOrEmpty(input))
+				{
+					bool ok = true;
+
+					for (int i = 0; i < input.Length; i++)
+					{
+						char c = input[i];
+						if (!char.IsLetter(c) && c != ' ')
+						{
+							ok = false;
+							break;
+						}
+					}
+
+					if (ok)
+						return input;
+				}
+
+				Console.WriteLine("Du måste ange en sträng med bara bokstäver och mellanslag");
+			}
+		}
+
+		public static string ReadYesNo(string text)
+		{
+			while (true)
+			{
+				Console.Write(text);
+				string? input = Console.ReadLine();
+
+				if (!string.IsNullOrEmpty(input))
+				{
+					input = input.ToLower();
+					if (input == "j" || input == "n")
+						return input;
+				}
+				Console.WriteLine("Felaktigt val!");
+			}
+		}
+
+		public static void ClearScreen(string heading)
+		{
+			Console.Clear();
+			Console.WriteLine(heading + "\n---------------------------------");
+		}
+
+		public static void PrintOptions()
+		{
+			Console.WriteLine("Gör ett val");
+			string[] options = new string[] { "Ändra ett namn", "Lista alla namn", "Avsluta" };
+			for (int i = 0; i < options.Length; i++)
+			{
+				Console.WriteLine($"{i + 1}. {options[i]}");
+			}
+		}
+	}
+}
---
+++ 12.18 - Poker/Program.cs
@@ -0,0 +1,125 @@
+﻿using App.IO;
+
+namespace _12._18___Poker
+{
+	internal class Program
+	{
+		static Random rnd = new Random();
+		static void Main(string[] args)
+		{
+			List<string> deck = new List<string>();
+			List<string> myHand = new List<string>();
+			int numberOfCards = 5;
+
+			ConsoleHelper.ClearScreen("POKER");
+			AddCardsToDeck(deck);
+			AddCardsToMyHand(deck, myHand, numberOfCards);
+			PrintMyHand(myHand);
+			Console.WriteLine();
+			var choice = ConsoleHelper.ReadYesNo("Vill du kasta några kort (j/n)? ");
+			if (choice == "j")
+			{
+				var numberOfCardsInHand = myHand.Count;
+				var indexesToRemove = ConsoleHelper.ReadStringIntPoker("Vilka index vill du kasta? ", numberOfCardsInHand - 1);
+				RemoveCardsFromMyHand(myHand, indexesToRemove);
+				int numberOfCardsRemoved = indexesToRemove.Length;
+				AddCardsToMyHand(deck, myHand, numberOfCardsRemoved);
+				Console.WriteLine();
+				PrintMyHand(myHand);
+			}
+		}
+		static void AddCardsToDeck(List<string> deck)
+		{
+			deck.Add("Hjärter 2");
+			deck.Add("Hjärter 3");
+			deck.Add("Hjärter 4");
+			deck.Add("Hjärter 5");
+			deck.Add("Hjärter 6");
+			deck.Add("Hjärter 7");
+			deck.Add("Hjärter 8");
+			deck.Add("Hjärter 9");
+			deck.Add("Hjärter 10");
+			deck.Add("Hjärter knekt");
+			deck.Add("Hjärter dam");
+			deck.Add("Hjärter kung");
+			deck.Add("Hjärter ess");
+			deck.Add("Ruter 2");
+			deck.Add("Ruter 3");
+			deck.Add("Ruter 4");
+			deck.Add("Ruter 5");
+			deck.Add("Ruter 6");
+			deck.Add("Ruter 7");
+			deck.Add("Ruter 8");
+			deck.Add("Ruter 9");
+			deck.Add("Ruter 10");
+			deck.Add("Ruter knekt");
+			deck.Add("Ruter dam");
+			deck.Add("Ruter kung");
+			deck.Add("Ruter ess");
+			deck.Add("Spader 2");
+			deck.Add("Spader 3");
+			deck.Add("Spader 4");
+			deck.Add("Spader 5");
+			deck.Add("Spader 6");
+			deck.Add("Spader 7");
+			deck.Add("Spader 8");
+			deck.Add("Spader 9");
+			deck.Add("Spader 10");
+			deck.Add("Spader knekt");
+			deck.Add("Spader dam");
+			deck.Add("Spader kung");
+			deck.Add("Spader ess");
+			deck.Add("Klöver 2");
+			deck.Add("Klöver 3");
+			deck.Add("Klöver 4");
+			deck.Add("Klöver 5");
+			deck.Add("Klöver 6");
+			deck.Add("Klöver 7");
+			deck.Add("Klöver 8");
+			deck.Add("Klöver 9");
+			deck.Add("Klöver 10");
+			deck.Add("Klöver knekt");
+			deck.Add("Klöver dam");
+			deck.Add("Klöver kung");
+			deck.Add("Klöver ess");
+		}
+		static void AddCardsToMyHand( List<string> deck, List<string> myHand, int noOfCards)
+		{
+			for (int i = 0; i < noOfCards; i++)
+			{
+				var randomIndex = rnd.Next(0, deck.Count);
+				var randomCard = deck[randomIndex];
+				myHand.Add(randomCard);
+				RemoveCardFromDeck(deck, randomCard);
+			}
+		}
+		static void RemoveCardFromDeck(List<string> deck, string card)
+		{
+			deck.Remove(card);
+		}
+		static void PrintMyHand(List<string> myHand)
+		{
+			for (int i = 0; i < myHand.Count; i++)
+			{
+				Console.WriteLine($"[{i}] {myHand[i]}");
+			}
+		}
+		static void RemoveCardsFromMyHand(List<string> myHand, string indexes)
+		{
+			List<int> sortedIndexes = new List<int>();
+
+			foreach (var c in indexes)
+			{
+				sortedIndexes.Add(c - '0');
+			}
+
+			sortedIndexes.Sort();
+			sortedIndexes.Reverse();
+
+			foreach (var c in sortedIndexes)
+			{
+				myHand.RemoveAt(c);
+			}
+		}
+	}
+}
