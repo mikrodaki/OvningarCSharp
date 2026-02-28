51d5c0efeddbfecfeb55c97daaf99d892f80969b
Rikard Carlsson <rikard.carlsson@iths.se>
2026-02-28 16:43:46 +0100

    Tal

---
+++ 12.16 - Tärning/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _12._16___Tärning
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Random rnd = new Random();
+			List<int> dices = new List<int>();
+			var numberOfFours = 0;
+			var numberOfThrows = 50;
+
+			for (int i = 0; i < numberOfThrows; i++)
+			{
+				int diceThrow = rnd.Next(1, 7);
+				dices.Add(diceThrow);
+			}
+
+			foreach (var diceThrow in dices)
+			{
+				if (diceThrow == 4)
+					numberOfFours++;
+			}
+
+			double percentageOfFours = (double)numberOfFours / numberOfThrows * 100;
+
+			Console.WriteLine($"Procentandelen fyror: {percentageOfFours}%");
+		}
+	}
+}
---
+++ 12.17 - Memorering/Program.cs
@@ -0,0 +1,43 @@
+﻿using App.IO;
+
+namespace _12._17___Memorering
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Random rnd = new Random();
+			List<int> randomNumbers = new List<int>();
+			int numberOfNumbers = 10;
+			bool playerLose = false;
+
+			for (int i = 0; i < numberOfNumbers; i++)
+			{
+				randomNumbers.Add(rnd.Next(0, 11)); // 0..10
+			}
+
+			ConsoleHelper.ClearScreen("Memorera följande tal. Tryck på ENTER när du är redo.");
+
+			foreach (int n in randomNumbers)
+				Console.Write(n + " ");
+
+			while (Console.ReadKey(true).Key != ConsoleKey.Enter) { }
+
+			ConsoleHelper.ClearScreen("Ange talen du memorerade");
+
+			for (int i = 0; i < numberOfNumbers; i++)
+			{
+				int input = ConsoleHelper.ReadInt($"Vilket tal var nr {i + 1}? ");
+				if (input == randomNumbers[i])
+					Console.WriteLine("Rätt!");
+				else
+				{
+					playerLose = true;
+					break;
+				}
+			}
+
+			Console.WriteLine(playerLose ? "Fel! Du förlorade." : "Du vann!");
+		}
+	}
+}
