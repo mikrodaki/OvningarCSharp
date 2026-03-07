6eccba72c1f756a5f099b53c26b0a18c7793dce0
Rikard Carlsson <rikard.carlsson@iths.se>
2026-03-07 23:06:48 +0100

    Pacman

---
+++ 13.14 - Pacman ghosts/Ghost.cs
@@ -0,0 +1,49 @@
+﻿namespace Ghost
+{
+	public class Ghost
+	{
+		private int x;
+		private int y;
+		private ConsoleColor color;
+		private int eyePos;
+		private List<string> eyes = new List<string>();
+
+
+		public Ghost(int x, int y, ConsoleColor color, int eyePosition)
+		{
+			this.x = x;
+			this.y = y;
+			this.color = color;
+			this.eyePos = eyePosition;
+			eyes.Add("▀ ");
+			eyes.Add(" ▀");
+			eyes.Add(" ▄");
+			eyes.Add("▄ ");
+		}
+
+		public void Draw()
+		{
+			Console.ForegroundColor = color;
+			Console.SetCursorPosition(x, y);
+			Console.WriteLine(" ▄▄▄▄▄");
+			Console.SetCursorPosition(x, y + 1);
+			Console.WriteLine("▐  █  ▌");
+			Console.SetCursorPosition(x, y + 2);
+			Console.WriteLine("███████");
+			Console.SetCursorPosition(x, y + 3);
+			Console.WriteLine("▀ ▀ ▀ ▀");
+			Console.ForegroundColor = ConsoleColor.Blue;
+			Console.BackgroundColor = ConsoleColor.White;
+			Console.SetCursorPosition(x + 1, y + 1);
+			Console.WriteLine(eyes[eyePos]);
+			Console.SetCursorPosition(x + 4, y + 1);
+			Console.WriteLine(eyes[eyePos]);
+			Console.BackgroundColor = ConsoleColor.Black;
+		}
+
+		public void ChangeEyePosition()
+		{
+			eyePos = (eyePos + 1) % 4;
+		}
+	}
+}
---
+++ 13.14 - Pacman ghosts/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Ghost
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<Ghost> ghosts = new List<Ghost>();
+			int x = 35;
+			int y = 10;
+			int spaceBetween = 10;
+
+			Console.Clear();
+			Console.CursorVisible = false;
+
+			ghosts.Add(new Ghost(x, y, ConsoleColor.Red, 0));
+			ghosts.Add(new Ghost(x += spaceBetween, y, ConsoleColor.Green, 3));
+			ghosts.Add(new Ghost(x += spaceBetween, y, ConsoleColor.Cyan, 1));
+			ghosts.Add(new Ghost(x += spaceBetween, y, ConsoleColor.DarkYellow, 2));
+			ghosts.Add(new Ghost(x += spaceBetween, y, ConsoleColor.Red, 0));
+			ghosts.Add(new Ghost(x += spaceBetween, y, ConsoleColor.Magenta, 2));
+
+
+			foreach (Ghost g in ghosts)
+			{
+				g.Draw();
+			}
+
+			while (true)
+			{
+				foreach (Ghost g in ghosts)
+				{
+					g.ChangeEyePosition();
+					Thread.Sleep(1000);
+					g.Draw();
+				}
+			}
+
+			Console.ReadKey();
+
+
+			/*
+
+
+			int eyePos = 0;
+			
+
+			*/
+		}
+	}
+}
