c6c9dfe34c77725a615c4ca5d97d71f1329faf85
Rikard Carlsson <rikard.carlsson@iths.se>
2026-03-09 20:09:29 +0100

    Maze

---
+++ 13.5 - Maze/Constants.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Maze
+{
+    class Constants
+    {
+        public static int X_SCREEN_POS = 46;
+        public static int Y_SCREEN_POS = 4;
+
+        public const int IDLE = 0;
+        public const int UP = 1;
+        public const int DOWN = 2;
+        public const int LEFT = 3;
+        public const int RIGHT = 4;
+    }
+}
---
+++ 13.5 - Maze/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Maze
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.CursorVisible = false;
+			Maze maze = new Maze();
+			maze.Draw();
+			Player player = new Player();
+			while (true) 
+			{ 
+				player.Draw();
+				Thread.Sleep(200);
+				player.Delete();
+				player.Move(maze);
+			}
+		}
+
+
+		/*
+		 * ReadKeys
+		 * 
+		 * Reads arrow keys and store the next direction
+		 * 
+		 */
+		static void ReadKeys()
+		{
+			ConsoleKeyInfo keyInfo = new ConsoleKeyInfo();
+
+			if (Console.KeyAvailable)
+				keyInfo = Console.ReadKey();
+
+			switch (keyInfo.Key)
+			{
+				case ConsoleKey.UpArrow:
+					//Player.SetNextDirection(Constants.UP);
+					break;
+
+				case ConsoleKey.DownArrow:
+					//Player.SetNextDirection(Constants.DOWN);
+					break;
+
+				case ConsoleKey.LeftArrow:
+					//Player.SetNextDirection(Constants.LEFT);
+					break;
+
+				case ConsoleKey.RightArrow:
+					//Player.SetNextDirection(Constants.RIGHT);
+					break;
+
+				default:
+					break;
+			}
+		}
+	}
+}
