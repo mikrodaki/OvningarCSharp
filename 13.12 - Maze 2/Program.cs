697b121ecdea05257b522479e7e7eaa2429758a6
RikardCarlsson <RikardCarlsson@W11-4W8GJS3>
2026-03-18 21:44:57 +0100

    Maze 2

--- 13.12 - Maze 2/Program.cs
+++ 13.12 - Maze 2/Program.cs
@@ -12,7 +12,7 @@
             Player player = new Player();
             //Enemy enemy = new Enemy(5, 1, Constants.DOWN);
             for (int i = 0; i < enemyX.Length; i++)
-            { 
+            {
                 enemies.Add(new Enemy(enemyX[i], enemyY[i], enemyDir[i]));
             }
 
@@ -29,28 +29,40 @@
                  *  Ändra direction-variablen i player till nextDirection
                  *  om det går. */
                 player.ChangeDirection(maze);
-                //enemy.ChangeDirection(maze.maze);
-                foreach (Enemy enemy in enemies) 
+                foreach (Enemy enemy in enemies)
                 {
                     enemy.ChangeDirection(maze.maze);
                 }
                 player.Delete();
-                //enemy.Delete();
                 foreach (Enemy enemy in enemies)
                 {
                     enemy.Delete();
                 }
                 player.Move(maze);
-                //enemy.Move(maze.maze);
                 foreach (Enemy enemy in enemies)
                 {
                     enemy.Move(maze.maze);
                 }
                 player.Draw();
-                //enemy.Draw();
+                foreach (Enemy enemy in enemies)
+                {
+                    if (enemy.x == player.x && enemy.y == player.y)
+                    {
+                        Hit(enemy.x, enemy.y);
+                        player = new Player();
+                    }
+                }
                 foreach (Enemy enemy in enemies)
                 {
                     enemy.Draw();
+                }
+                foreach(Enemy enemy in enemies) 
+                {
+                    if (enemy.x == player.x && enemy.y == player.y) 
+                    { 
+                        Hit(enemy.x,enemy.y);
+                        player = new Player();
+                    }
                 }
                 if (maze.maze[player.y, player.x] == 2)
                 {
@@ -100,6 +112,38 @@
                 default:
                     break;
             }
+        }
+
+        /*
+        * Hit
+        * 
+        * Hit loops and shows X and @ five times 
+        * at the specified coordinate.
+        * 
+        */
+        static void Hit(int x, int y)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                WriteAt(x, y, ConsoleColor.White, 'X');
+                WriteAt(x, y, ConsoleColor.Red, '@');
+            }
+        }
+
+
+        /*
+         * WriteAt
+         * 
+         * WriteAt - help method that writes a char at
+         * a specific coordinate in a specified color
+         * 
+         */
+        static void WriteAt(int x, int y, ConsoleColor color, char c)
+        {
+            Console.ForegroundColor = color;
+            Console.SetCursorPosition(x + Constants.X_SCREEN_POS, y + Constants.Y_SCREEN_POS);
+            Console.Write(c);
+            System.Threading.Thread.Sleep(80);
         }
     }
 }
