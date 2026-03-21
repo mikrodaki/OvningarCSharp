445cb5f3372b9d51eab608a9206fb176ca983a24
Rikard Carlsson <rikard.carlsson@iths.se>
2026-03-21 19:09:54 +0100

    Binärsökning

---
+++ 14.6 - Linjär sökning/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace _14._6___Linjär_sökning
+{
+	internal class Program
+	{
+		static Random rnd = new Random();
+		static int vectorLength = 20;
+		static void Main(string[] args)
+		{
+			Console.CursorVisible = false;
+			for (int j = 0; j < 5; j++)
+			{
+				Console.Clear();
+				var numbers = PopulateVector(new int[vectorLength]);
+				var key = rnd.Next(vectorLength);
+				var result = LinearSearch(numbers, key);
+				Console.Write("LINJÄR SÖKNING \n------------------\nLISTAN: ");
+				bool keyFound = false;
+				for (int i = 0; i < numbers.Length; i++)
+				{
+					if (key == numbers[i] && !keyFound)
+					{
+						Console.ForegroundColor = ConsoleColor.Green;
+						Console.Write(numbers[i] + " ");
+						keyFound = true;
+						Console.ForegroundColor = ConsoleColor.White;
+					}
+					else
+						Console.Write(numbers[i] + " ");
+				}
+				Console.WriteLine("\n");
+				if (result.index != -1)
+					Console.WriteLine($"Talet {key} finns på index {result.index} i listan. " +
+						$"Det tog {result.steps} iterationer för att hitta talet.");
+				else
+					Console.WriteLine($"Talet {key} finns inte med i listan. " +
+						$"Det tog {result.steps} iterationer för att komma fram till det.");
+				Console.ReadKey();
+			}
+		}
+
+		static (int index, int steps) LinearSearch(int[] numbers, int key)
+		{
+			int steps = 0;
+			if (numbers.Length == 0)
+				return (-1, steps);
+			for (int i = 0; i < numbers.Length; i++)
+			{
+				steps++;
+				if (numbers[i] == key)
+					return (i, steps);
+			}
+			return (-1, steps);
+		}
+
+		static int[] PopulateVector(int[] numbers)
+		{
+			for (int i = 0; i < numbers.Length; i++)
+			{
+				numbers[i] = rnd.Next(vectorLength);
+			}
+			return numbers;
+		}
+	}
+}
---
+++ 14.7 - Binärsökning/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace Binärsökning
+{
+	internal class Program
+	{
+		static Random rnd = new Random();
+		static int vectorLength = 20;
+		static void Main(string[] args)
+		{
+			Console.CursorVisible = false;
+			for (int j = 0; j < 5; j++)
+			{
+				Console.Clear();
+				var numbers = PopulateVector(new List<int>());
+				var numbersUnsorted = new List<int>(numbers);
+				var key = rnd.Next(vectorLength);
+				var result = BinarySearch(numbers, key);
+				Console.Write("BINÄR SÖKNING \n------------------\nLISTAN: ");
+				bool keyFound = false;
+				int lineBreak = 0;
+				foreach (int number in numbersUnsorted)
+				{
+					if (key == number && !keyFound)
+					{
+						Console.ForegroundColor = ConsoleColor.Green;
+						Console.Write(number + " ");
+						keyFound = true;
+						Console.ForegroundColor = ConsoleColor.White;
+					}
+					else
+						Console.Write(number + " ");
+					lineBreak++;
+				}
+				Console.WriteLine("\n");
+				if (result.index != -1)
+					Console.WriteLine($"Talet {key} finns på index {numbersUnsorted.IndexOf(key)} i listan. " +
+						$"Det tog {result.steps} iterationer för att hitta talet.");
+				else
+					Console.WriteLine($"Talet {key} finns inte med i listan. " +
+						$"Det tog {result.steps} iterationer för att komma fram till det.");
+				Console.ReadKey();
+			}
+		}
+
+		static (int index, int steps) BinarySearch(List<int> numbers, int key)
+		{
+			int steps = 0;
+			if (numbers.Count == 0)
+				return (-1, steps);
+			numbers.Sort();
+			int first = 0;
+			int last = numbers.Count - 1;
+			while (first <= last)
+			{
+				steps++;
+				int mid = (first + last) / 2;
+				if (key > numbers[mid])
+					first = mid + 1;
+				else if (key < numbers[mid])
+					last = mid - 1;
+				else
+					return (mid, steps);
+			}
+			return (-1, steps);
+		}
+
+		static List<int> PopulateVector(List<int> numbers)
+		{
+			for (int i = 0; i < vectorLength; i++)
+			{
+				numbers.Add(rnd.Next(vectorLength));
+			}
+			return numbers;
+		}
+	}
+}
