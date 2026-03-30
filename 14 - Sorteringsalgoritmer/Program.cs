6636f322836298be16071935069bef4144aa0ebd
Rikard Carlsson <rikard.carlsson@iths.se>
2026-03-30 21:16:12 +0200

    h

--- 14 - Sorteringsalgoritmer/Program.cs
+++ 14 - Sorteringsalgoritmer/Program.cs
@@ -1,57 +1,22 @@
-﻿namespace _14___Sorteringsalgoritmer
+﻿using SortingAlgorithms;
+
+namespace _14___Sorteringsalgoritmer
 {
 	internal class Program
 	{
 		static Random rnd = new Random();
 		static void Main(string[] args)
 		{
-			int[] numbers = new int[20];
+			int[] numbers = new int[5];
 			GenerateRandomList(numbers);
 			//Console.WriteLine("List generated.");
 			for (int i = 0; i < numbers.Length; i++)
 				Console.Write(numbers[i] + " ");
-			SelectionSort(numbers);
+			Sorter.SelectionSort(numbers);
 			//Console.WriteLine("List sorted.");
 			Console.WriteLine();
 			for (int i = 0; i < numbers.Length; i++)
 				Console.Write(numbers[i] + " ");
-		}
-
-		static void BubbleSort(int[] numbers)
-		{
-			int max = numbers.Length - 1;
-			for (int i = 0; i < max; i++)
-			{
-				int nrLeft = max - i;
-				for (int j = 0; j < nrLeft; j++)
-				{
-					if (numbers[j] > numbers[j + 1])
-						Swap(ref numbers[j], ref numbers[j + 1]);
-				}
-			}
-		}
-
-		static void SelectionSort(int[] numbers)
-		{
-			for (int i = 0; i < numbers.Length - 1; i++)
-			{
-				int minIndex = i;
-
-				for (int j = i + 1; j < numbers.Length; j++)
-				{
-					if ((numbers[j] < numbers[minIndex]))
-						minIndex = j;
-				}
-				if (minIndex != i)
-					Swap(ref numbers[i], ref numbers[minIndex]);
-			}
-		}
-
-		static void Swap(ref int a, ref int b)
-		{
-			int temp = a;
-			a = b;
-			b = temp;
 		}
 
 		public static void GenerateRandomList(int[] list)
---
+++ 14 - Sorteringsalgoritmer/Sorter.cs
@@ -0,0 +1,63 @@
+﻿namespace SortingAlgorithms
+{
+	public static class Sorter
+	{
+		public static void BubbleSort(int[] numbers)
+		{
+			int max = numbers.Length - 1;
+			for (int i = 0; i < max; i++)
+			{
+				int nrLeft = max - i;
+				for (int j = 0; j < nrLeft; j++)
+				{
+					if (numbers[j] > numbers[j + 1])
+						Swap(ref numbers[j], ref numbers[j + 1]);
+				}
+			}
+		}
+
+		public static void SelectionSort(int[] numbers)
+		{
+			for (int i = 0; i < numbers.Length - 1; i++)
+			{
+				int minIndex = i;
+
+				for (int j = i + 1; j < numbers.Length; j++)
+				{
+					if ((numbers[j] < numbers[minIndex]))
+						minIndex = j;
+				}
+				if (minIndex != i)
+					Swap(ref numbers[i], ref numbers[minIndex]);
+			}
+		}
+
+		public static void InsertionSort(int[] numbers)
+		{
+			for (int i = 1; i < numbers.Length; i++)
+			{
+				int current = numbers[i];
+				int j = i - 1;
+
+				while (j >= 0 && numbers[j] > current)
+				{
+					numbers[j + 1] = numbers[j];
+					j--;
+				}
+				numbers[j + 1] = current;
+			}
+		}
+
+		public static void QuickSort(int[] numbers) 
+		{ 
+			
+		}
+
+		static void Swap(ref int a, ref int b)
+		{
+			int temp = a;
+			a = b;
+			b = temp;
+		}
+	}
+}
