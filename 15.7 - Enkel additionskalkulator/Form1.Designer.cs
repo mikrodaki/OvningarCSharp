eab2ce685c86baeba0598391da9b95a33ad79cbe
Rikard Carlsson <rikard.carlsson@iths.se>
2026-05-27 20:50:22 +0200

    alskdjflka

--- 15.7 - Enkel additionskalkulator/Form1.Designer.cs
+++ 15.7 - Enkel additionskalkulator/Form1.Designer.cs
@@ -20,238 +20,262 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            txtResult = new TextBox();
-            button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
-            button4 = new Button();
-            button5 = new Button();
-            button6 = new Button();
-            button7 = new Button();
-            button8 = new Button();
-            button9 = new Button();
-            buttonClear = new Button();
-            buttonAddition = new Button();
-            buttonEquals = new Button();
-            button0 = new Button();
-            buttonDEL = new Button();
-            buttonComma = new Button();
-            SuspendLayout();
-            // 
-            // txtResult
-            // 
-            txtResult.Enabled = false;
-            txtResult.Font = new Font("Cascadia Code", 16F);
-            txtResult.Location = new Point(29, 39);
-            txtResult.Name = "txtResult";
-            txtResult.Size = new Size(234, 38);
-            txtResult.TabIndex = 0;
-            txtResult.TextAlign = HorizontalAlignment.Right;
-            // 
-            // button1
-            // 
-            button1.Font = new Font("Tahoma", 12F);
-            button1.Location = new Point(29, 97);
-            button1.Name = "button1";
-            button1.Size = new Size(74, 65);
-            button1.TabIndex = 1;
-            button1.Text = "1";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
-            // 
-            // button2
-            // 
-            button2.Font = new Font("Tahoma", 12F);
-            button2.Location = new Point(109, 97);
-            button2.Name = "button2";
-            button2.Size = new Size(74, 65);
-            button2.TabIndex = 2;
-            button2.Text = "2";
-            button2.UseVisualStyleBackColor = true;
-            button2.Click += button2_Click;
-            // 
-            // button3
-            // 
-            button3.Font = new Font("Tahoma", 12F);
-            button3.Location = new Point(189, 97);
-            button3.Name = "button3";
-            button3.Size = new Size(74, 65);
-            button3.TabIndex = 3;
-            button3.Text = "3";
-            button3.UseVisualStyleBackColor = true;
-            button3.Click += button3_Click;
-            // 
-            // button4
-            // 
-            button4.Font = new Font("Tahoma", 12F);
-            button4.Location = new Point(29, 168);
-            button4.Name = "button4";
-            button4.Size = new Size(74, 65);
-            button4.TabIndex = 4;
-            button4.Text = "4";
-            button4.UseVisualStyleBackColor = true;
-            button4.Click += button4_Click;
-            // 
-            // button5
-            // 
-            button5.Font = new Font("Tahoma", 12F);
-            button5.Location = new Point(109, 169);
-            button5.Name = "button5";
-            button5.Size = new Size(74, 65);
-            button5.TabIndex = 5;
-            button5.Text = "5";
-            button5.UseVisualStyleBackColor = true;
-            button5.Click += button5_Click;
-            // 
-            // button6
-            // 
-            button6.Font = new Font("Tahoma", 12F);
-            button6.Location = new Point(189, 169);
-            button6.Name = "button6";
-            button6.Size = new Size(74, 65);
-            button6.TabIndex = 6;
-            button6.Text = "6";
-            button6.UseVisualStyleBackColor = true;
-            button6.Click += button6_Click;
-            // 
-            // button7
-            // 
-            button7.Font = new Font("Tahoma", 12F);
-            button7.Location = new Point(29, 239);
-            button7.Name = "button7";
-            button7.Size = new Size(74, 65);
-            button7.TabIndex = 7;
-            button7.Text = "7";
-            button7.UseVisualStyleBackColor = true;
-            button7.Click += button7_Click;
-            // 
-            // button8
-            // 
-            button8.Font = new Font("Tahoma", 12F);
-            button8.Location = new Point(109, 240);
-            button8.Name = "button8";
-            button8.Size = new Size(74, 65);
-            button8.TabIndex = 8;
-            button8.Text = "8";
-            button8.UseVisualStyleBackColor = true;
-            button8.Click += button8_Click;
-            // 
-            // button9
-            // 
-            button9.Font = new Font("Tahoma", 12F);
-            button9.Location = new Point(189, 240);
-            button9.Name = "button9";
-            button9.Size = new Size(74, 65);
-            button9.TabIndex = 9;
-            button9.Text = "9";
-            button9.UseVisualStyleBackColor = true;
-            button9.Click += button9_Click;
-            // 
-            // buttonClear
-            // 
-            buttonClear.Font = new Font("Tahoma", 12F);
-            buttonClear.Location = new Point(29, 310);
-            buttonClear.Name = "buttonClear";
-            buttonClear.Size = new Size(74, 65);
-            buttonClear.TabIndex = 10;
-            buttonClear.Text = "C";
-            buttonClear.UseVisualStyleBackColor = true;
-            buttonClear.Click += buttonClear_Click;
-            // 
-            // buttonAddition
-            // 
-            buttonAddition.Font = new Font("Tahoma", 12F);
-            buttonAddition.Location = new Point(189, 310);
-            buttonAddition.Name = "buttonAddition";
-            buttonAddition.Size = new Size(74, 65);
-            buttonAddition.TabIndex = 11;
-            buttonAddition.Text = "+";
-            buttonAddition.UseVisualStyleBackColor = true;
-            buttonAddition.Click += buttonAddition_Click;
-            // 
-            // buttonEquals
-            // 
-            buttonEquals.Font = new Font("Tahoma", 12F);
-            buttonEquals.Location = new Point(29, 381);
-            buttonEquals.Name = "buttonEquals";
-            buttonEquals.Size = new Size(74, 65);
-            buttonEquals.TabIndex = 12;
-            buttonEquals.Text = "=";
-            buttonEquals.UseVisualStyleBackColor = true;
-            buttonEquals.Click += buttonEquals_Click;
-            // 
-            // button0
-            // 
-            button0.Font = new Font("Tahoma", 12F);
-            button0.Location = new Point(109, 310);
-            button0.Name = "button0";
-            button0.Size = new Size(74, 65);
-            button0.TabIndex = 13;
-            button0.Text = "0";
-            button0.UseVisualStyleBackColor = true;
-            button0.Click += button0_Click;
-            // 
-            // buttonDEL
-            // 
-            buttonDEL.Font = new Font("Tahoma", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            buttonDEL.Location = new Point(109, 381);
-            buttonDEL.Name = "buttonDEL";
-            buttonDEL.Size = new Size(74, 65);
-            buttonDEL.TabIndex = 14;
-            buttonDEL.Text = "DEL";
-            buttonDEL.UseVisualStyleBackColor = true;
-            buttonDEL.Click += buttonDEL_Click;
-            // 
-            // buttonComma
-            // 
-            buttonComma.Font = new Font("Tahoma", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            buttonComma.Location = new Point(189, 381);
-            buttonComma.Name = "buttonComma";
-            buttonComma.Size = new Size(74, 65);
-            buttonComma.TabIndex = 15;
-            buttonComma.Text = ",";
-            buttonComma.UseVisualStyleBackColor = true;
-            buttonComma.Click += buttonComma_Click;
-            // 
-            // Form1
-            // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
-            AutoScaleMode = AutoScaleMode.Font;
-            BackColor = SystemColors.WindowFrame;
-            ClientSize = new Size(291, 491);
-            Controls.Add(buttonComma);
-            Controls.Add(buttonDEL);
-            Controls.Add(button0);
-            Controls.Add(buttonEquals);
-            Controls.Add(buttonAddition);
-            Controls.Add(buttonClear);
-            Controls.Add(button9);
-            Controls.Add(button8);
-            Controls.Add(button7);
-            Controls.Add(button6);
-            Controls.Add(button5);
-            Controls.Add(button4);
-            Controls.Add(button3);
-            Controls.Add(button2);
-            Controls.Add(button1);
-            Controls.Add(txtResult);
-            Name = "Form1";
-            Text = "Calculator";
-            ResumeLayout(false);
-            PerformLayout();
-        }
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			components = new System.ComponentModel.Container();
+			txtResult = new TextBox();
+			button1 = new Button();
+			button2 = new Button();
+			button3 = new Button();
+			button4 = new Button();
+			button5 = new Button();
+			button6 = new Button();
+			button7 = new Button();
+			button8 = new Button();
+			button9 = new Button();
+			buttonClear = new Button();
+			buttonAddition = new Button();
+			buttonEquals = new Button();
+			button0 = new Button();
+			buttonDEL = new Button();
+			buttonComma = new Button();
+			errorTimer = new System.Windows.Forms.Timer(components);
+			SuspendLayout();
+			// 
+			// txtResult
+			// 
+			txtResult.Enabled = false;
+			txtResult.Font = new Font("Cascadia Code", 16F);
+			txtResult.Location = new Point(36, 49);
+			txtResult.Margin = new Padding(4);
+			txtResult.Name = "txtResult";
+			txtResult.Size = new Size(292, 45);
+			txtResult.TabIndex = 0;
+			txtResult.TextAlign = HorizontalAlignment.Right;
+			// 
+			// button1
+			// 
+			button1.Font = new Font("Tahoma", 12F);
+			button1.Location = new Point(36, 121);
+			button1.Margin = new Padding(4);
+			button1.Name = "button1";
+			button1.Size = new Size(92, 81);
+			button1.TabIndex = 1;
+			button1.Text = "1";
+			button1.UseVisualStyleBackColor = true;
+			button1.Click += button1_Click;
+			// 
+			// button2
+			// 
+			button2.Font = new Font("Tahoma", 12F);
+			button2.Location = new Point(136, 121);
+			button2.Margin = new Padding(4);
+			button2.Name = "button2";
+			button2.Size = new Size(92, 81);
+			button2.TabIndex = 2;
+			button2.Text = "2";
+			button2.UseVisualStyleBackColor = true;
+			button2.Click += button2_Click;
+			// 
+			// button3
+			// 
+			button3.Font = new Font("Tahoma", 12F);
+			button3.Location = new Point(236, 121);
+			button3.Margin = new Padding(4);
+			button3.Name = "button3";
+			button3.Size = new Size(92, 81);
+			button3.TabIndex = 3;
+			button3.Text = "3";
+			button3.UseVisualStyleBackColor = true;
+			button3.Click += button3_Click;
+			// 
+			// button4
+			// 
+			button4.Font = new Font("Tahoma", 12F);
+			button4.Location = new Point(36, 210);
+			button4.Margin = new Padding(4);
+			button4.Name = "button4";
+			button4.Size = new Size(92, 81);
+			button4.TabIndex = 4;
+			button4.Text = "4";
+			button4.UseVisualStyleBackColor = true;
+			button4.Click += button4_Click;
+			// 
+			// button5
+			// 
+			button5.Font = new Font("Tahoma", 12F);
+			button5.Location = new Point(136, 211);
+			button5.Margin = new Padding(4);
+			button5.Name = "button5";
+			button5.Size = new Size(92, 81);
+			button5.TabIndex = 5;
+			button5.Text = "5";
+			button5.UseVisualStyleBackColor = true;
+			button5.Click += button5_Click;
+			// 
+			// button6
+			// 
+			button6.Font = new Font("Tahoma", 12F);
+			button6.Location = new Point(236, 211);
+			button6.Margin = new Padding(4);
+			button6.Name = "button6";
+			button6.Size = new Size(92, 81);
+			button6.TabIndex = 6;
+			button6.Text = "6";
+			button6.UseVisualStyleBackColor = true;
+			button6.Click += button6_Click;
+			// 
+			// button7
+			// 
+			button7.Font = new Font("Tahoma", 12F);
+			button7.Location = new Point(36, 299);
+			button7.Margin = new Padding(4);
+			button7.Name = "button7";
+			button7.Size = new Size(92, 81);
+			button7.TabIndex = 7;
+			button7.Text = "7";
+			button7.UseVisualStyleBackColor = true;
+			button7.Click += button7_Click;
+			// 
+			// button8
+			// 
+			button8.Font = new Font("Tahoma", 12F);
+			button8.Location = new Point(136, 300);
+			button8.Margin = new Padding(4);
+			button8.Name = "button8";
+			button8.Size = new Size(92, 81);
+			button8.TabIndex = 8;
+			button8.Text = "8";
+			button8.UseVisualStyleBackColor = true;
+			button8.Click += button8_Click;
+			// 
+			// button9
+			// 
+			button9.Font = new Font("Tahoma", 12F);
+			button9.Location = new Point(236, 300);
+			button9.Margin = new Padding(4);
+			button9.Name = "button9";
+			button9.Size = new Size(92, 81);
+			button9.TabIndex = 9;
+			button9.Text = "9";
+			button9.UseVisualStyleBackColor = true;
+			button9.Click += button9_Click;
+			// 
+			// buttonClear
+			// 
+			buttonClear.Font = new Font("Tahoma", 12F);
+			buttonClear.Location = new Point(36, 388);
+			buttonClear.Margin = new Padding(4);
+			buttonClear.Name = "buttonClear";
+			buttonClear.Size = new Size(92, 81);
+			buttonClear.TabIndex = 10;
+			buttonClear.Text = "C";
+			buttonClear.UseVisualStyleBackColor = true;
+			buttonClear.Click += buttonClear_Click;
+			// 
+			// buttonAddition
+			// 
+			buttonAddition.Font = new Font("Tahoma", 12F);
+			buttonAddition.Location = new Point(236, 388);
+			buttonAddition.Margin = new Padding(4);
+			buttonAddition.Name = "buttonAddition";
+			buttonAddition.Size = new Size(92, 81);
+			buttonAddition.TabIndex = 11;
+			buttonAddition.Text = "+";
+			buttonAddition.UseVisualStyleBackColor = true;
+			buttonAddition.Click += buttonAddition_Click;
+			// 
+			// buttonEquals
+			// 
+			buttonEquals.Font = new Font("Tahoma", 12F);
+			buttonEquals.Location = new Point(36, 476);
+			buttonEquals.Margin = new Padding(4);
+			buttonEquals.Name = "buttonEquals";
+			buttonEquals.Size = new Size(92, 81);
+			buttonEquals.TabIndex = 12;
+			buttonEquals.Text = "=";
+			buttonEquals.UseVisualStyleBackColor = true;
+			buttonEquals.Click += buttonEquals_Click;
+			// 
+			// button0
+			// 
+			button0.Font = new Font("Tahoma", 12F);
+			button0.Location = new Point(136, 388);
+			button0.Margin = new Padding(4);
+			button0.Name = "button0";
+			button0.Size = new Size(92, 81);
+			button0.TabIndex = 13;
+			button0.Text = "0";
+			button0.UseVisualStyleBackColor = true;
+			button0.Click += button0_Click;
+			// 
+			// buttonDEL
+			// 
+			buttonDEL.Font = new Font("Tahoma", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+			buttonDEL.Location = new Point(136, 476);
+			buttonDEL.Margin = new Padding(4);
+			buttonDEL.Name = "buttonDEL";
+			buttonDEL.Size = new Size(92, 81);
+			buttonDEL.TabIndex = 14;
+			buttonDEL.Text = "DEL";
+			buttonDEL.UseVisualStyleBackColor = true;
+			buttonDEL.Click += buttonDEL_Click;
+			// 
+			// buttonComma
+			// 
+			buttonComma.Font = new Font("Tahoma", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+			buttonComma.Location = new Point(236, 476);
+			buttonComma.Margin = new Padding(4);
+			buttonComma.Name = "buttonComma";
+			buttonComma.Size = new Size(92, 81);
+			buttonComma.TabIndex = 15;
+			buttonComma.Text = ",";
+			buttonComma.UseVisualStyleBackColor = true;
+			buttonComma.Click += buttonComma_Click;
+			// 
+			// errorTimer
+			// 
+			errorTimer.Interval = 2000;
+			errorTimer.Tick += errorTimer_Tick;
+			// 
+			// Form1
+			// 
+			AutoScaleDimensions = new SizeF(10F, 25F);
+			AutoScaleMode = AutoScaleMode.Font;
+			BackColor = SystemColors.WindowFrame;
+			ClientSize = new Size(356, 587);
+			Controls.Add(buttonComma);
+			Controls.Add(buttonDEL);
+			Controls.Add(button0);
+			Controls.Add(buttonEquals);
+			Controls.Add(buttonAddition);
+			Controls.Add(buttonClear);
+			Controls.Add(button9);
+			Controls.Add(button8);
+			Controls.Add(button7);
+			Controls.Add(button6);
+			Controls.Add(button5);
+			Controls.Add(button4);
+			Controls.Add(button3);
+			Controls.Add(button2);
+			Controls.Add(button1);
+			Controls.Add(txtResult);
+			Margin = new Padding(4);
+			Name = "Form1";
+			Text = "Calculator";
+			ResumeLayout(false);
+			PerformLayout();
+		}
 
-        #endregion
+		#endregion
 
-        private TextBox txtResult;
+		private TextBox txtResult;
         private Button button1;
         private Button button2;
         private Button button3;
@@ -267,5 +291,6 @@
         private Button button0;
         private Button buttonDEL;
         private Button buttonComma;
-    }
+		private System.Windows.Forms.Timer errorTimer;
+	}
 }
--- 15.7 - Enkel additionskalkulator/Form1.cs
+++ 15.7 - Enkel additionskalkulator/Form1.cs
@@ -1,122 +1,143 @@
 namespace _15._7___Enkel_additionskalkulator
 {
-    public partial class Form1 : Form
-    {
-        List<decimal> numbers = new List<decimal>();
-        public Form1()
-        {
-            InitializeComponent();
-        }
+	public partial class Form1 : Form
+	{
+		List<decimal> numbers = new List<decimal>();
+		public Form1()
+		{
+			InitializeComponent();
+		}
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            txtResult.Text += "1";
-        }
+		private void button1_Click(object sender, EventArgs e)
+		{
+			txtResult.Text += "1";
+		}
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            txtResult.Text += "2";
-        }
+		private void button2_Click(object sender, EventArgs e)
+		{
+			txtResult.Text += "2";
+		}
 
-        private void button3_Click(object sender, EventArgs e)
-        {
-            txtResult.Text += "3";
-        }
+		private void button3_Click(object sender, EventArgs e)
+		{
+			txtResult.Text += "3";
+		}
 
-        private void button4_Click(object sender, EventArgs e)
-        {
-            txtResult.Text += "4";
-        }
+		private void button4_Click(object sender, EventArgs e)
+		{
+			txtResult.Text += "4";
+		}
 
-        private void button5_Click(object sender, EventArgs e)
-        {
-            txtResult.Text += "5";
-        }
+		private void button5_Click(object sender, EventArgs e)
+		{
+			txtResult.Text += "5";
+		}
 
-        private void button6_Click(object sender, EventArgs e)
-        {
-            txtResult.Text += "6";
-        }
+		private void button6_Click(object sender, EventArgs e)
+		{
+			txtResult.Text += "6";
+		}
 
-        private void button7_Click(object sender, EventArgs e)
-        {
-            txtResult.Text += "7";
-        }
+		private void button7_Click(object sender, EventArgs e)
+		{
+			txtResult.Text += "7";
+		}
 
-        private void button8_Click(object sender, EventArgs e)
-        {
-            txtResult.Text += "8";
-        }
+		private void button8_Click(object sender, EventArgs e)
+		{
+			txtResult.Text += "8";
+		}
 
-        private void button9_Click(object sender, EventArgs e)
-        {
-            txtResult.Text += "9";
-        }
+		private void button9_Click(object sender, EventArgs e)
+		{
+			txtResult.Text += "9";
+		}
 
-        private void buttonAddition_Click(object sender, EventArgs e)
-        {
-            AddNumberToList();
-            txtResult.ResetText();
-        }
+		private void buttonAddition_Click(object sender, EventArgs e)
+		{
+			if (AddNumberToList())
+				txtResult.ResetText();
+			else 
+			{
+				txtResult.Text = "Error";
+				errorTimer.Start();
+			}
+		}
 
-        private void buttonClear_Click(object sender, EventArgs e)
-        {
-            numbers.Clear();
-            txtResult.ResetText();
-        }
+		private void buttonClear_Click(object sender, EventArgs e)
+		{
+			numbers.Clear();
+			txtResult.ResetText();
+		}
 
-        private void buttonEquals_Click(object sender, EventArgs e)
-        {
-            if (string.IsNullOrWhiteSpace(txtResult.Text) && numbers.Count == 0)
-                return;
-            if (!string.IsNullOrWhiteSpace(txtResult.Text))
-                AddNumberToList();
-            var result = CalculateSum();
-            txtResult.Text = result.ToString("0.##");
-            numbers.Clear();
-        }
+		private void buttonEquals_Click(object sender, EventArgs e)
+		{
+			if (string.IsNullOrWhiteSpace(txtResult.Text) && numbers.Count == 0)
+				return;
+			if (!string.IsNullOrWhiteSpace(txtResult.Text))
+				if (!AddNumberToList())
+				{
+					txtResult.Text = "Error";
+					errorTimer.Start();
+					return;
+				}
+			var result = CalculateSum();
+			txtResult.Text = result.ToString("0.##");
+			numbers.Clear();
+		}
 
 
 
-        private void button0_Click(object sender, EventArgs e)
-        {
-            txtResult.Text += "0";
-        }
+		private void button0_Click(object sender, EventArgs e)
+		{
+			txtResult.Text += "0";
+		}
 
-        private decimal CalculateSum()
-        {
-            //if (numbers.Count == 0)
-            //    return 0;
-            //int result = 0;
-            //foreach (int n in numbers)
-            //{
-            //    result += n;
-            //}
-            //return result;
-            return numbers.Sum();
-        }
+		private decimal CalculateSum()
+		{
+			//if (numbers.Count == 0)
+			//    return 0;
+			//int result = 0;
+			//foreach (int n in numbers)
+			//{
+			//    result += n;
+			//}
+			//return result;
+			return numbers.Sum();
+		}
 
-        private void AddNumberToList()
-        {
-            if (decimal.TryParse(txtResult.Text, out decimal number))
-                numbers.Add(number);
-        }
+		private bool AddNumberToList()
+		{
+			if (decimal.TryParse(txtResult.Text, out decimal number))
+			{
+				numbers.Add(number);
+				return true;
+			}
+			return false;
+		}
 
-        private void buttonDEL_Click(object sender, EventArgs e)
-        {
-            if (txtResult.Text.Length > 0)
-                txtResult.Text = txtResult.Text.Substring(0, txtResult.Text.Length - 1);
-        }
+		private void buttonDEL_Click(object sender, EventArgs e)
+		{
+			if (txtResult.Text.Length > 0)
+				txtResult.Text = txtResult.Text.Substring(0, txtResult.Text.Length - 1);
+		}
 
-        private void buttonComma_Click(object sender, EventArgs e)
-        {
-            if (!txtResult.Text.Contains(","))
-            {
-                if (string.IsNullOrEmpty(txtResult.Text))
-                    txtResult.Text = "0,";
-                else
-                    txtResult.Text += ",";
-            }
-        }
-    }
+		private void buttonComma_Click(object sender, EventArgs e)
+		{
+			if (!txtResult.Text.Contains(","))
+			{
+				if (string.IsNullOrEmpty(txtResult.Text))
+					txtResult.Text = "0,";
+				else
+					txtResult.Text += ",";
+			}
+		}
+
+		private void errorTimer_Tick(object sender, EventArgs e)
+		{
+			txtResult.ResetText();
+			errorTimer.Stop();
+			numbers.Clear();
+		}
+	}
 }
