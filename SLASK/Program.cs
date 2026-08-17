971b5192f9afe3550bf3ba25536d1fc28d8cda13
Rikard Carlsson <rikard.carlsson@up.se>
2026-08-17 10:59:50 +0200

    slask

---
+++ SLASK/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("██████╗  ██╗");
+        Console.WriteLine("██╔══██╗███║");
+        Console.WriteLine("██████╔╝╚██║");
+        Console.WriteLine("██╔═══╝  ██║");
+        Console.WriteLine("██║      ██║");
+        Console.WriteLine("╚═╝      ╚═╝");
+
+        Console.WriteLine("> dotnet run");
+        Console.WriteLine();
+        Console.WriteLine("PPPPPP    111111");
+        Console.WriteLine("PP   PP      11");
+        Console.WriteLine("PPPPPP       11");
+        Console.WriteLine("PP           11");
+        Console.WriteLine("PP        111111");
+        Console.WriteLine();
+        Console.WriteLine("> _");
+
+        Console.ForegroundColor = ConsoleColor.Green;
+
+        Console.WriteLine("class Programming1");
+        Console.WriteLine("{");
+        Console.WriteLine("    static void Main()");
+        Console.WriteLine("    {");
+        Console.WriteLine("        Console.Write(\"P1\");");
+        Console.WriteLine("    }");
+        Console.WriteLine("}");
+    }
+}
